36c1a69f2079eb9ffa29d9dba48c98fe77e50ca5
Zombizz97 <thibaultribes.tr@gmail.com>
2023-12-06 12:19:52 +0100

    Feat (header) : make header in (tabs)/_layout.tsx

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,6 +1,6 @@
 import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { Link, Tabs } from 'expo-router';
-import { Pressable, useColorScheme } from 'react-native';
+import { Tabs } from 'expo-router';
+import { useColorScheme, View, Image, StyleSheet} from 'react-native';
 
 import Colors from '../../constants/Colors';
 
@@ -27,20 +27,26 @@ export default function TabLayout() {
         options={{
           title: 'Accueil',
           tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
-          headerRight: () => (
-            <Link href="/modal" asChild>
-              <Pressable>
-                {({ pressed }) => (
-                  <FontAwesome
-                    name="info-circle"
-                    size={25}
-                    color={Colors[colorScheme ?? 'light'].text}
-                    style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
-                  />
-                )}
-              </Pressable>
-            </Link>
-          ),
+          header: () => (
+              <View style={styles.headerBar}>
+                  <Image source={require('../../assets/images/LOGO-01.png')} style={styles.logo} />
+              </View>
+          )
+          // Header for a modal button
+          // headerRight: () => (
+          //   <Link href="/modal" asChild>
+          //     <Pressable>
+          //       {({ pressed }) => (
+          //         <FontAwesome
+          //           name="info-circle"
+          //           size={25}
+          //           color={Colors[colorScheme ?? 'light'].text}
+          //           style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
+          //         />
+          //       )}
+          //     </Pressable>
+          //   </Link>
+          // ),
         }}
       />
       <Tabs.Screen
@@ -48,8 +54,29 @@ export default function TabLayout() {
         options={{
           title: 'Détails',
           tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          header: () => (
+              <View style={styles.headerBar}>
+                  <Image source={require('../../assets/images/LOGO-01.png')} style={styles.logo} />
+              </View>
+          )
         }}
       />
     </Tabs>
   );
 }
+
+const styles = StyleSheet.create({
+    logo: {
+        resizeMode: 'contain',
+        height: '80%',
+        width: '80%'
+    },
+    headerBar: {
+        flexDirection: 'row',
+        alignItems: 'flex-end',
+        justifyContent: 'center',
+        height: 130,
+        backgroundColor: '#fff',
+        width: '100%'
+    }
+});
--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,17 +1,17 @@
-const tintColorLight = '#2f95dc';
+const tintColorLight = '#fff';
 const tintColorDark = '#fff';
 
 export default {
   light: {
-    text: '#000',
-    background: '#fff',
+    text: '#fff',
+    background: '#0F016A',
     tint: tintColorLight,
     tabIconDefault: '#ccc',
     tabIconSelected: tintColorLight,
   },
   dark: {
     text: '#fff',
-    background: '#000',
+    background: '#0F016A',
     tint: tintColorDark,
     tabIconDefault: '#ccc',
     tabIconSelected: tintColorDark,
